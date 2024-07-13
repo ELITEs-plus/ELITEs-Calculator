a52591ef0de52b81724d1f485cf731feff5ca4dc
Pasworn Chanchai <hello.elitespllus@gmail.com>
2024-07-13 17:23:47 -0400

    Add basic plus functionality
    - Basic add feature is working
    
    Next Step:
    - Add chain plus
    - Fix the reset style of the plus button
    - Fix displaying number
    
    commit #005

--- display.js
+++ display.js
@@ -1,11 +1,10 @@
 let displayNumber = [];
+let operationHolder = [];
 let toDisplay = document.getElementById("display-num")
 let num1 = 0;
 let num2 = 0;
 
-let displayFrame = document.getElementById("display-frame__display");
-
-// define all clickable keys - Number
+// Define all clickable keys - Number
 let key_zero = document.getElementById("zero");
 let key_one = document.getElementById("one");
 let key_two = document.getElementById("two");
@@ -33,7 +32,7 @@ let key_backspace = document.getElementById("backspace");
 let key_clear = document.getElementById("clear");
 let key_allClear = document.getElementById("allClear");
 
-// define eventListener - for adding number
+// Define eventListener - for adding number
 key_zero.addEventListener("click", () => addToDisplay(displayNumber, 0));
 key_one.addEventListener("click", () => addToDisplay(displayNumber, 1));
 key_two.addEventListener("click", () => addToDisplay(displayNumber, 2));
@@ -52,6 +51,56 @@ key_clear.addEventListener("click", () => clearNumber());
 key_allClear.addEventListener("click", () => clearNumber());
 key_plus.addEventListener("click", () => pressAdd());
 
+key_equal.addEventListener("click", () => pressEqual());
+
+// Define operation object
+const operator = {
+
+    plus: false,
+    minus: false,
+    multiply: false,
+    divide: false,
+
+    resetOperation: function() {
+        for (let key in this) {
+            if (this.hasOwnProperty(key) && typeof this[key] !== "function") {
+                this[key] = false;
+            }
+        }
+    },
+
+    setOperation: function(operation) {
+        if (this.hasOwnProperty(operation)) {
+            this.resetOperation();
+            this[operation] = true;
+        }
+    },
+
+    checkOperation: function() {
+        for (let key in this) {
+            if (this.hasOwnProperty(key) && typeof this[key] !== "function") {
+                if (this[key]=true) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+    
+    plusOperation: function(num1, num2) {
+        return num1 + num2;
+    },
+
+    runOperation: function(num1, num2) {
+        switch (true) {
+            case this.plus :
+                console.log("Its add!")
+                return this.plusOperation(num1, num2);
+        }
+    },
+};
+
+// Display functions
 const addToDisplay  = (arr, val) => {
     // Check max length
     if (arr.length === 11) {
@@ -88,13 +137,37 @@ const clearNumber = () => {
     toDisplay.innerHTML = "0";
 }
 
+const resetStyle = (key) => {
+    key.style.border = "0.1px solid rgba(46, 70, 98, 0.50)";
+    key.style.color = "#2E4662";
+    key.style.boxShadow = "3px 5px 4px rgba(0, 0, 0, 0.25), inset -6px -2px 6px rgba(46, 70, 98, 0.70), inset 1px -2px 4px rgba(46, 70, 98, 0.25), inset 3px 4px 4px rgba(46, 70, 98, 0.25)";
+};
+
+// Math operations
 const pressAdd = () => {
     num1 = Number(displayNumber.join(""));
+    displayNumber = [];
+    operationHolder.push(key_plus);
     key_plus.style.border = "2.5px solid #FFFFFF";
     key_plus.style.color = "#FFFFFF";
     key_plus.style.boxShadow = "3px 5px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px rgba(46, 70, 98, 0.70), inset -1px -2px 4px rgba(46, 70, 98, 0.25), inset 1px -2px 4px rgba(46, 70, 98, 0.10)"
-    console.log(num1)
 }
+
+const pressEqual = () => {
+    if (operator.checkOperation()) {
+
+        if (operationHolder.length !== 0) {
+            resetStyle(operationHolder[0]);
+        }
+
+        let answer = 0;
+        num2 = Number(displayNumber.join(""));
+        answer = operator.runOperation(num1, num2);
+        toDisplay.innerHTML = answer.toString();
+        displayNumber = [];
+        operationHolder = [];
+    }
+};
 
 
 // const convertToScientific = (string) => {
